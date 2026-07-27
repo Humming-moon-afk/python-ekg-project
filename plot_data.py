822977f7e89bf901060d4f96ffdf6096b2f242e0
Humming-Moon-afk <milian.loechner@gmail.com>
2026-07-27 11:35:21 +0200

    feat: read ekg data from csv

---
+++ plot_data.py
@@ -0,0 +1,10 @@
+with open("ekg_daten.csv", "r") as file:
+    times = []
+    signals = []
+    line = file.readline()
+    for line in file:
+        lineContent = line.split(",")
+        timeContent = int(lineContent[0])
+        signalContent = int(lineContent[1])
+        times.append(timeContent)
+        signals.append(signalContent)
