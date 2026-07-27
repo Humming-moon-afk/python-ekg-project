6f35813675109df8d7019fcf08ba506488bdd02b
Humming-Moon-afk <milian.loechner@gmail.com>
2026-07-27 10:28:51 +0200

    feat: generate synthetic ekg data into csv

--- generate_data.py
+++ generate_data.py
@@ -2,3 +2,6 @@ import random
 
 with open("ekg_daten.csv", "w") as file:
     file.write("time,signal\n")
+    for i in range(0,61):
+        signal = random.randint(60, 120)
+        file.write(f"{i},{signal}\n")
